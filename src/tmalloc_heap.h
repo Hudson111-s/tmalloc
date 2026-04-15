90b28a4703299462d21641d77173c22c8370dbb4
Hudson111-s <145516182+Hudson111-s@users.noreply.github.com>
2026-04-15 18:23:35 -0400

    Fix heap_up bug

--- src/tmalloc_heap.h
+++ src/tmalloc_heap.h
@@ -13,6 +13,6 @@ typedef struct TimedMalloc {
 
 size_t get_heap_size();
 TimedMalloc heap_peek();
-void heap_free(void *ptr);
+int heap_free(void *ptr);
 int heap_push(TimedMalloc tm);
 TimedMalloc heap_pop();
